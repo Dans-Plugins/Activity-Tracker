ecd2b6029e9610444911212f83b51fd9586ed7fe
dmccoystephenson <dmccoystephenson@gmail.com>
2021-10-05 16:26:56 -0600

    Added some constants to StorageManager.java

--- src/main/java/dansplugins/activitytracker/managers/StorageManager.java
+++ src/main/java/dansplugins/activitytracker/managers/StorageManager.java
@@ -4,6 +4,10 @@ public class StorageManager {
 
     private static StorageManager instance;
 
+    private final static String FILE_PATH = "./plugins/ActivityTracker/";
+    private final static String ACTIVITY_RECORDS_FILE_NAME = "activityRecords.json";
+    private final static String SESSIONS_FILE_NAME = "sessions.json";
+
     private StorageManager() {
 
     }
