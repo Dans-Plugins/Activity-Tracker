8620a89cd70a26f9cbca3270c574aecb1b8751a4
dmccoystephenson <dmccoystephenson@gmail.com>
2021-10-05 11:16:22 -0600

    Created StorageManager.java

--- src/main/java/dansplugins/activitytracker/ActivityTracker.java
+++ src/main/java/dansplugins/activitytracker/ActivityTracker.java
@@ -1,16 +1,24 @@
 package dansplugins.activitytracker;
 
+import dansplugins.activitytracker.managers.StorageManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class ActivityTracker extends JavaPlugin {
 
+    private static ActivityTracker instance;
+
+    public static ActivityTracker getInstance() {
+        return instance;
+    }
+
     @Override
     public void onEnable() {
-
+        instance = this;
+        StorageManager.getInstance().load();
     }
 
     @Override
     public void onDisable() {
-
+        StorageManager.getInstance().save();
     }
 }
---
+++ src/main/java/dansplugins/activitytracker/managers/StorageManager.java
@@ -0,0 +1,26 @@
+package dansplugins.activitytracker.managers;
+
+public class StorageManager {
+
+    private static StorageManager instance;
+
+    private StorageManager() {
+
+    }
+
+    public static StorageManager getInstance() {
+        if (instance == null) {
+            instance = new StorageManager();
+        }
+        return instance;
+    }
+
+    public void save() {
+        // TODO: implement
+    }
+
+    public void load() {
+        // TODO: implement
+    }
+
+}
