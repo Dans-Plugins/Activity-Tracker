c650429843885acc9c0a7aa91d765310a83f6582
dmccoystephenson <dmccoystephenson@gmail.com>
2021-10-05 11:11:33 -0600

    initial commit

---
+++ src/main/java/dansplugins/activitytracker/ActivityTracker.java
@@ -0,0 +1,16 @@
+package dansplugins.activitytracker;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ActivityTracker extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
