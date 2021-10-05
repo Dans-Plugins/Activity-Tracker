d2bbc0c4366c1549b96c613a4cfbdd22b3f10742
dmccoystephenson <dmccoystephenson@gmail.com>
2021-10-05 12:10:44 -0600

    Created PersistentData.java

---
+++ src/main/java/dansplugins/activitytracker/data/PersistentData.java
@@ -0,0 +1,43 @@
+package dansplugins.activitytracker.data;
+
+import dansplugins.activitytracker.objects.ActivityRecord;
+import org.bukkit.entity.Player;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
+public class PersistentData {
+
+    private static PersistentData instance;
+
+    private ArrayList<ActivityRecord> activityRecords = new ArrayList<>();
+
+    private PersistentData() {
+
+    }
+
+    public static PersistentData getInstance() {
+        if (instance == null) {
+            instance = new PersistentData();
+        }
+        return instance;
+    }
+
+    public ArrayList<ActivityRecord> getActivityRecords() {
+        return activityRecords;
+    }
+
+    public ActivityRecord getActivityRecord(Player player) {
+        return getActivityRecord(player.getUniqueId());
+    }
+
+    public ActivityRecord getActivityRecord(UUID playerUUID) {
+        for (ActivityRecord record : activityRecords) {
+            if (record.getPlayerUUID().equals(playerUUID)) {
+                return record;
+            }
+        }
+        return null;
+    }
+
+}
--- src/main/java/dansplugins/activitytracker/managers/ActivityRecordManager.java
+++ src/main/java/dansplugins/activitytracker/managers/ActivityRecordManager.java
@@ -18,6 +18,6 @@ public class ActivityRecordManager {
 
 
     public void assignActivityRecordToPlayerIfNecessary(Player player) {
-        // TODO: implement
+        if ()
     }
 }
