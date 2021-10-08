98089be10e33ad0c0c491170e80955289af33968
dmccoystephenson <dmccoystephenson@gmail.com>
2021-10-08 01:22:00 -0600

    Finished implementing top command.

--- src/main/java/dansplugins/activitytracker/commands/TopCommand.java
+++ src/main/java/dansplugins/activitytracker/commands/TopCommand.java
@@ -2,6 +2,8 @@ package dansplugins.activitytracker.commands;
 
 import dansplugins.activitytracker.managers.ActivityRecordManager;
 import dansplugins.activitytracker.objects.ActivityRecord;
+import dansplugins.activitytracker.utils.UUIDChecker;
+import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 
 import java.util.ArrayList;
@@ -11,6 +13,10 @@ public class TopCommand implements ICommand {
     @Override
     public boolean execute(CommandSender sender) {
         ArrayList<ActivityRecord> records = ActivityRecordManager.getInstance().getTopTenRecords();
+        for (ActivityRecord record : records) {
+            String playerName = UUIDChecker.getInstance().findPlayerNameBasedOnUUID(record.getPlayerUUID());
+            sender.sendMessage(ChatColor.AQUA + playerName + " - " + String.format("%.2f", record.getHoursSpent()));
+        }
         return false;
     }
 
