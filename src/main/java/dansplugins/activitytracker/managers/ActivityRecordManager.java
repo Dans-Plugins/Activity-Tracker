57494c623054d5adcdf0eb4faf181b75f16ab1ce
dmccoystephenson <dmccoystephenson@gmail.com>
2021-10-08 13:57:35 -0600

    Added some debug messages.

--- src/main/java/dansplugins/activitytracker/managers/ActivityRecordManager.java
+++ src/main/java/dansplugins/activitytracker/managers/ActivityRecordManager.java
@@ -3,6 +3,8 @@ package dansplugins.activitytracker.managers;
 import dansplugins.activitytracker.data.PersistentData;
 import dansplugins.activitytracker.factories.ActivityRecordFactory;
 import dansplugins.activitytracker.objects.ActivityRecord;
+import dansplugins.activitytracker.utils.Logger;
+import dansplugins.activitytracker.utils.UUIDChecker;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 
@@ -52,10 +54,12 @@ public class ActivityRecordManager {
     }
 
     public ActivityRecord getTopRecord(ArrayList<ActivityRecord> toIgnore) {
+        Logger.getInstance().log("Attempting to get top record, ignoring " + toIgnore.size() + " records.");
         ActivityRecord toReturn = null;
         double max = 0;
         for (ActivityRecord record : PersistentData.getInstance().getActivityRecords()) {
             if (toIgnore.contains(record)) {
+                Logger.getInstance().log("Record for " + record.getPlayerUUID().toString() + " is in the ignore list.");
                 continue;
             }
             if (record.getTotalHoursSpent() > max) {
@@ -63,6 +67,7 @@ public class ActivityRecordManager {
                 max = record.getTotalHoursSpent();
             }
         }
+        Logger.getInstance().log("Record for " + toReturn.getPlayerUUID() + " is the top record currently. " + toIgnore.size() + " records were ignored when performing this search.");
         return toReturn;
     }
 }
